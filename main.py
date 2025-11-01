d2ccbde5c02b4006d5cc890fde879e852339c173
IlliaMiletskyi <illia.miletskyi@oa.edu.ua>
2025-11-01 23:49:31 +0200

    Solution v1.2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open("players.json", "rb") as file:
+    with open("players.json", "r") as file:
         players = json.load(file)
 
     for name in players:
